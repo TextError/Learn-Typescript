9eba8ce5e594575cc97b00fe62e534844bec395b
TextError <savinraul1983@yahoo.com>
2020-02-06 16:31:37 +0200

    When to use annotation

--- src/components/Typescript/ts/annotations.ts
+++ src/components/Typescript/ts/annotations.ts
@@ -24,3 +24,25 @@ let isObject: { x: number; y: string } = { x: 10, y: 'ten' };
 
 // Function
 const myFunc: (i: number) => void = (i: number) => console.log(i);
+
+// When to use annotations
+// 1. Function that return the `any` type
+const toJson = '{"age": 10; "name": "Jhon"}';
+const person: { age: number; name: string } = JSON.parse(toJson);
+console.log(person) // {"age": 10; "name": "Jhon"}
+
+// 2. When we declare a variable on one line and initalizate it later
+let isArrayOfWords = ['red', 'green', 'blue'];
+let toBeGreen: boolean;
+
+for (let i = 0; i < isArrayOfWords.length; i++) {
+  if(isArrayOfWords[i] === 'green') toBeGreen = true;
+}
+
+// 3. Variables whose type cannot be inferred correctly
+let isNewArrayOfNumbers = [1, 2, 3];
+let isNumberAboveZero: boolean | number = false;
+
+for (let i = 0; i < isNewArrayOfNumbers.length; i++) {
+  if(isNewArrayOfNumbers[i] > 0) isNumberAboveZero = isNewArrayOfNumbers[i];
+}
